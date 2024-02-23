039c33b8ca8750fb8ef7c84a75aa0e2bf4f2a5f6
jtaravel <jtaravel@student.42.fr>
2024-02-23 21:09:54 +0100

    WIP ls -R

--- ft_ls.h
+++ ft_ls.h
@@ -2,10 +2,19 @@
 
 #define FT_LS_H
 
+#include <stdbool.h>
+
 typedef struct s_files
 {
-	char	**toopen
-}	t_files
+	char	**toopen;
+	char	**toread;
+	char	**files;
+	bool	l;
+	bool	a;
+	bool	R;
+	bool	r;
+	bool	t;
+}	t_files;
 
 
 #endif
--- main.c
+++ main.c
@@ -1,9 +1,12 @@
 #include <sys/types.h>
+#include <errno.h>
+#include <sys/stat.h>
 #include <dirent.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
 #include <stddef.h>
+#include "ft_ls.h"
 
 char	*ft_strdup(char *str);
 
@@ -37,7 +40,18 @@ void	ft_putstr(char *str)
 		write(1, &str[i], 1);
 		i++;
 	}
-	write(1, "\n", 1);
+}
+
+void	ft_putstr_fd(char *str, int fd)
+{
+	int	i;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		write(fd, &str[i], 1);
+		i++;
+	}
 }
 
 char	**ft_swap(int ac, char **av)
@@ -46,10 +60,10 @@ char	**ft_swap(int ac, char **av)
 	int		j;
 	char	*swap;
 
-	i = 1;
+	i = 0;
 	while (i != ac)
 	{
-		j = 1;
+		j = 0;
 		while (j < ac)
 		{
 			if (ft_strcmp(av[i], av[j]) < 0)
@@ -65,6 +79,11 @@ char	**ft_swap(int ac, char **av)
 	return (av);
 }
 
+void	ft_putchar_fd(char c, int fd)
+{
+	write(fd, &c, 1);
+}
+
 int	check_options(char *opt)
 {
 	int	i;
@@ -73,91 +92,305 @@ int	check_options(char *opt)
 	while (opt[++i])
 	{
 		if (opt[i] == '-' || opt[i] == 'l' || opt[i] == 'R' || opt[i] == 'a' || opt[i] == 'r' || opt[i] == 't')
-			i++;
+			;
 		else
+		{
+			ft_putstr_fd("ft_ls: invalid option -- '", 2);
+			ft_putchar_fd(opt[i], 2);
+			ft_putstr_fd("'\n", 2);
+			ft_putstr_fd("try 'ft_ls --help' for more information.\n", 2);
 			return (1);
+		}
 	}
+	return (0);
+}
+
+int	is_a_file(char *str)
+{
+	DIR    *dir_ptr;
+
+	dir_ptr = opendir(str);
+	if (dir_ptr == NULL)
+	{
+		if (errno == ENOTDIR)
+			return (1);
+		return (0);
+	}
+	if (closedir(dir_ptr))
+		perror("closedir");
+	return (0);
+}
+
+int	exec_ls_no_args(void)
+{
+	struct dirent 	*tab;
+	DIR 		*actu = opendir("./");
+	char 		**store;
+	int 		size = 0;
+
+	tab = readdir(actu);
+	while (tab)
+	{
+		size++;
+		tab = readdir(actu);
+	}
+	store = malloc(sizeof(char *) * size + 1);
+	actu = opendir("./");
+	tab = readdir(actu);
+	int i = 0;
+	while (tab)
+	{
+		store[i] = ft_strdup(tab->d_name);
+		i++;
+		tab = readdir(actu);
+	}
+	store = ft_swap(size, store);
+	i = 0;
+	while (store[i])
+	{
+		if (store[i][0] != '.')
+			printf("%s\n", store[i]);
+		i++;
+	}
+	return (0);
+}
+
+int	init_data(char **av, t_files *data)
+{
+	int	i = 1;
+	int	c = 0;
+	int	k = 0;
+	int	f = 0;
+	int	n;
+	int	j;
+	char	**toopen;
+	char	**toread;
+	char	**files;
+	char	**options;
+	struct	stat	*tmp;
+
+	while (av[i])
+	{
+		if (opendir(av[i]))
+			c++;
+		else if (is_a_file(av[i]))
+			f++;
+		else if (check_options(av[i]))
+		{
+			return (1);
+		}
+		i++;
+	}
+	toopen = malloc(sizeof(char *) + c + 1);
+	toread = malloc(sizeof(char *) + c + 1);
+	files = malloc(sizeof(char *) + f + 1);
+	options = malloc(sizeof(char *) + (i - c) + 1);
+	i = 1;
+	j = 0;
+	f = 0;
+	n = 0;
+	while (av[i])
+	{
+		if (opendir(av[i]))
+		{
+			toopen[j] = ft_strdup(av[i]);
+			toread[j] = ft_strdup(av[i]);
+			j++;
+		}
+		else if (is_a_file(av[i]))
+		{
+			files[f] = ft_strdup(av[i]);
+			f++;
+		}
+		else
+		{
+			options[n] = ft_strdup(av[i] + 1);
+			n++;
+		}
+		i++;
+	}
+	toopen[j] = 0;
+	files[f] = 0;
+	options[n] = 0;
+	data->toopen = toopen;
+	data->toopen = ft_swap(j, data->toopen);
+	data->toread = toread;
+	data->toread = ft_swap(j, data->toread);
+	data->files = files;
+	data->files = ft_swap(f, data->files);
+	j = 0;
+	n = 0;
+
+	data->R = false;
+	data->r = false;
+	data->l = false;
+	data->a = false;
+	data->t = false;
+	while (options[n])
+	{
+		i = 0;
+		while (options[n][i])
+		{
+			if (options[n][i] == 'R')
+				data->R = true;
+			if (options[n][i] == 'r')
+				data->r = true;
+			if (options[n][i] == 'l')
+				data->l = true;
+			if (options[n][i] == 'a')
+				data->a = true;
+			if (options[n][i] == 't')
+				data->t = true;
+			i++;
+		}
+		n++;
+	}
+	return (0);
+}
+
+int	tablen(char **tab)
+{
+	int	i = 0;
+
+	while (tab[i])
+		i++;
+	return (i);
+}
+
+void	freetab(char **tab)
+{
+	int	i = 0;
+
+	while (tab[i])
+	{
+		free(tab[i]);
+		i++;
+	}
+	free(tab);
+}
+
+void	add_dir_file(t_files *data, char *dir, int mode)
+{
+	if (mode == 0)
+	{
+		char		**newd;
+		char		**newf;
+		int		i = 0;
+		DIR		*tmp = opendir(dir);
+		struct	dirent	*dirs = readdir(tmp);
+		int		c = 0;
+		int		f = 0;
+
+		while (dirs)
+		{
+			printf("i = %s\n", dirs->d_name);
+			if (opendir(dirs->d_name))
+				c++;
+			else if (is_a_file(dirs->d_name))
+				f++;
+			dirs = readdir(tmp);
+		}
+		newd = malloc(sizeof(char *) + tablen(data->toread + c) + 2);
+		newf = malloc(sizeof(char *) + tablen(data->files + f) + 2);
+		int k = 0;
+		int n = 0;
+		tmp = opendir(dir);
+		dirs = readdir(tmp);
+		while (dirs)
+		{
+			if (opendir(dirs->d_name) && dirs->d_name[0] != '.')
+			{
+				newd[k] = ft_strdup(dirs->d_name);
+				k++;
+			}
+			else if (dirs->d_name[0] != '.')
+			{
+				newf[n] = ft_strdup(dirs->d_name);
+				n++;
+			}
+			dirs = readdir(tmp);
+		}
+		newd[k + 1] = 0;
+		newf[n + 1] = 0;
+		freetab(data->toread);
+		data->toread = NULL;
+		data->toread = newd;
+		freetab(data->files);
+		data->files = NULL;
+		data->files = newf;
+	}
+	else
+	{
+		char	**new;
+		int	i = 0;
+
+		new = malloc(sizeof(char *) + tablen(data->files) + 2);
+		while (data->files[i])
+		{
+			new[i] = ft_strdup(data->files[i]);
+			i++;
+		}
+		new[i] = dir;
+		new[i + 1] = 0;
+		freetab(data->files);
+		data->files = new;
+	}
+}
+
+
+
+void	recursive(t_files *data)
+{
+	int	i = 0;
+	
+	while (data->toread[i])
+	{
+		printf("data = %s\n", data->toread[i]);
+		if (opendir(data->toread[i]))
+			add_dir_file(data, data->toread[i], 0);
+		//else if (is_a_file(data->toread[i]))
+		//	add_dir_file(data, data->toread[i], 1);
+		i++;
+	}
+
+}
+
+void	optionR(t_files *data)
+{
+	int i = 0;
+	
+	recursive(data);
+	while (data->files[i])
+	{
+		ft_putstr(data->files[i]);
+		if (data->files[i + 1])
+			ft_putstr_fd("  ", 1);
+		i++;
+	}
+	ft_putchar_fd('\n', 1);
+	ft_putchar_fd('\n', 1);
+
+
+}
+
+int	exec_ls_args(char **av)
+{
+	t_files data;
+	int ret = init_data(av, &data);
+	if (ret == 1)
+		return (1);
+	if (data.R == true)
+		optionR(&data);
 	return (0);
 }
 
 int	main(int ac, char **av)
 {
 	if (ac == 1)
-	{
-		struct dirent 	*tab;
-		DIR 		*actu = opendir("./");
-		char 		**store;
-		int 		size = 0;
-
-		tab = readdir(actu);
-		while (tab)
-		{
-			size++;
-			tab = readdir(actu);
-		}
-		store = malloc(sizeof(char *) * size + 1);
-		actu = opendir("./");
-		tab = readdir(actu);
-		int i = 0;
-		while (tab)
-		{
-			store[i] = ft_strdup(tab->d_name);
-			i++;
-			tab = readdir(actu);
-		}
-		store = ft_swap(size, store);
-		i = 0;
-		while (store[i])
-		{
-			if (store[i][0] != '.')
-				printf("%s\n", store[i]);
-			i++;
-		}
-	}
+		exec_ls_no_args();
 	else
 	{
-		int	i = 1;
-		int	c = 0;
-		int	k = 0;
-		int	j;
-		char	**toopen;
-
-		while (av[i])
-		{
-			if (opendir(av[i]))
-				c++;
-			else if (check_options(av[i]))
-			{
-				printf("Error\n");
-				return (1);
-			}
-			i++;
-		}
-		toopen = malloc(sizeof(char *) + c + 1);
-		i = 1;
-		j = 0;
-		while (av[i])
-		{
-			if (opendir(av[i]))
-			{
-				toopen[j] = ft_strdup(av[i]);
-				j++;
-			}
-			else
-				k++;
-			i++;
-		}
-		toopen[j] = 0;
-		j = 0;
-		while (toopen[j])
-		{
-			printf("%s\n", toopen[j]);
-			j++;
-		}
-
-
-
-		
+		if (exec_ls_args(av))
+			return (1);
 	}
-
 }
